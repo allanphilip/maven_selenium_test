6860522aafdec384e3f3c4d30dc37170ecdf93a6
allanphilip <allanphilip1595>
2025-05-13 13:29:11 +0530

    integrated testng to pom

--- src/test/java/mavenSelenium/seleniumMaventest/BaseClass.java
+++ src/test/java/mavenSelenium/seleniumMaventest/BaseClass.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
